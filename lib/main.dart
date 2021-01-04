a22c7b0024ae41caefa90fb88a97ca2282f1fa63
roimar urbano <51421744+urbanoprogramador@users.noreply.github.com>
2021-01-04 08:57:02 -0400

    gregando intersacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-        title:'Demo cuentas',
-        theme:ThemeData(
+  runApp(MaterialApp(
+      title: 'Demo cuentas',
+      theme: ThemeData(
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -15,49 +14,51 @@ void main() {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
-      
-      ),  home:MyApp()
-    )
-  );
+      ),
+      home: MyApp()));
 }
 
-class MyApp extends StatelessWidget{
-
-   List <Widget> crear(){
-    List <Widget> texto=[];
-    for( var i = 0 ; i <50; i++ ) { 
-      texto.add(
-         Container(padding:EdgeInsets.only(left: 10,top: 10,bottom: 10) , child:Text(' Posicion numero $i __ 2'))
-      );
+class MyApp extends StatelessWidget {
+  List<Widget> crear() {
+    List<Widget> texto = [];
+    for (var i = 0; i < 50; i++) {
+      texto.add(Container(
+          padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+          child: Text(' Posicion numero $i __ 2')));
     }
     return texto;
-  } 
+  }
 
   @override
   Widget build(BuildContext context) {
     //
-    List <Widget>  texto=[];
-    for( var i = 0 ; i <5; i++ ) { 
-      texto.add(
-        new Text(' Posicion numero $i')
-      );
+    List<Widget> texto = [];
+    for (var i = 0; i < 5; i++) {
+      texto.add(new Text(' Posicion numero $i'));
     }
-    final List lists=crear();
+    final List lists = crear();
     return new Scaffold(
       appBar: AppBar(
         title: Text('Statless Widget '),
       ),
       body: Container(
-        margin:EdgeInsets.only(top:25) ,
+        margin: EdgeInsets.only(top: 25),
         height: 400,
-        child: ListView(
-          children:[
-            MYcard(title: 'me gusta flutter',icon:Icons.fastfood_sharp ),
-            MYcard(title: 'me gusta flutter',icon:Icons.expand_more,myColor: Colors.purple, ),
-            MYcard(title: 'me gusta flutter',icon:Icons.fastfood_sharp, myColor: Colors.amber ),
-            MYcard(title: 'me gusta flutter 2',icon:Icons.favorite_border ),
-          ]
-        ),
+        child: ListView(children: [
+          MYcard(title: 'me gusta flutter 1', icon: Icons.fastfood_sharp),
+          MYcard(
+            title: 'me gusta flutter 2',
+            icon: Icons.expand_more,
+            myColor: Colors.purple,
+          ),
+          MYcard(
+              title: 'me gusta flutter 3',
+              icon: Icons.fastfood_sharp,
+              myColor: Colors.amber),
+          MYcard(
+              title: 'me gusta flutter 4444444444444',
+              icon: Icons.favorite_border),
+        ]),
       ),
     );
   }
@@ -67,22 +68,26 @@ class MYcard extends StatelessWidget {
   final String title;
   final IconData icon;
   final Color myColor;
-  
 
-  const MYcard({Key key,this.icon,this.title,this.myColor=Colors.deepOrange}) : super(key: key);
+  const MYcard(
+      {Key key, this.icon, this.title, this.myColor = Colors.deepOrange})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return GestureDetector(
+      onTap: () => {print(this.title)},
       child: Container(
-        padding: EdgeInsets.all( 5),
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: Colors.lightGreen[100],
+        ),
+        padding: EdgeInsets.all(5),
         child: Card(
           shadowColor: this.myColor,
           child: Column(
-            children: [
-              Text(this.title),
-              Icon(this.icon,color: this.myColor)
-            ],
+            children: [Text(this.title), Icon(this.icon, color: this.myColor)],
           ),
         ),
       ),
