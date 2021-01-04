0af08a2fa1ce4e44634d6b0b0365cac77e6ae64d
roimar urbano <51421744+urbanoprogramador@users.noreply.github.com>
2021-01-04 08:20:25 -0400

    statelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-       MaterialApp(
-         title:'Demo cuentas',
+    MaterialApp(
+        title:'Demo cuentas',
         theme:ThemeData(
         // This is the theme of your application.
         //
@@ -16,13 +16,76 @@ void main() {
         // is not restarted.
         primarySwatch: Colors.blue,
       
-      ),  home:new Center(
-          child:new Directionality(
-            textDirection: TextDirection.ltr,
-            child:new Text('hola mundo 21',style:TextStyle(backgroundColor: Colors.white,fontSize: 30,color: Colors.black)),
-          ),
-        )
-       )
-    );
+      ),  home:MyApp()
+    )
+  );
 }
 
+class MyApp extends StatelessWidget{
+
+   List <Widget> crear(){
+    List <Widget> texto=[];
+    for( var i = 0 ; i <50; i++ ) { 
+      texto.add(
+         Container(padding:EdgeInsets.only(left: 10,top: 10,bottom: 10) , child:Text(' Posicion numero $i __ 2'))
+      );
+    }
+    return texto;
+  } 
+
+  @override
+  Widget build(BuildContext context) {
+    //
+    List <Widget>  texto=[];
+    for( var i = 0 ; i <5; i++ ) { 
+      texto.add(
+        new Text(' Posicion numero $i')
+      );
+    }
+    final List lists=crear();
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Statless Widget '),
+      ),
+      body: Container(
+        margin:EdgeInsets.only(top:25) ,
+        height: 400,
+        child: ListView(
+          children:[
+            MYcard(title: 'me gusta flutter',icon:Icons.fastfood_sharp ),
+            MYcard(title: 'me gusta flutter',icon:Icons.expand_more,myColor: Colors.purple, ),
+            MYcard(title: 'me gusta flutter',icon:Icons.fastfood_sharp, myColor: Colors.amber ),
+            MYcard(title: 'me gusta flutter 2',icon:Icons.favorite_border ),
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+class MYcard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Color myColor;
+  
+
+  const MYcard({Key key,this.icon,this.title,this.myColor=Colors.deepOrange}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        padding: EdgeInsets.all( 5),
+        child: Card(
+          shadowColor: this.myColor,
+          child: Column(
+            children: [
+              Text(this.title),
+              Icon(this.icon,color: this.myColor)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
